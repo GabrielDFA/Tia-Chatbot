a75979f345f1f41371149ffb2271c85f03d6396f
GabrielDFA <gdfayal@gmail.com>
2024-06-06 13:29:28 +0700

    chatbot pbo

---
+++ Model/model.py
@@ -0,0 +1,40 @@
+import os
+import time
+import dotenv
+from openai import OpenAI
+
+dotenv.load_dotenv()
+
+api_key = os.getenv("API_KEY")
+assistant_id = os.getenv("ASSISTANT_ID")
+
+def load_openai_client_and_assistant():
+    client = OpenAI(api_key=api_key)
+    my_assistant = client.beta.assistants.retrieve(assistant_id)
+    thread = client.beta.threads.create()
+    return client, my_assistant, thread
+
+def wait_on_run(client, run, thread):
+    while run.status == "queued" or run.status == "in_progress":
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id,
+        )
+        time.sleep(0.5)
+    return run
+
+def get_assistant_response(client, assistant_thread, user_input=""):
+    message = client.beta.threads.messages.create(
+        thread_id=assistant_thread.id,
+        role="user",
+        content=user_input,
+    )
+    run = client.beta.threads.runs.create(
+        thread_id=assistant_thread.id,
+        assistant_id=assistant_id,
+    )
+    run = wait_on_run(client, run, assistant_thread)
+    messages = client.beta.threads.messages.list(
+        thread_id=assistant_thread.id, order="asc", after=message.id
+    )
+    return messages.data[0].content[0].text.value
---
+++ View/view.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from PIL import Image
+
+def initialize_session_state():
+    if 'user_input' not in st.session_state:
+        st.session_state.user_input = ''
+
+def submit():
+    st.session_state.user_input = st.session_state.query
+    st.session_state.query = ''
+
+def render_ui():
+    logo = Image.open("D:\d3if45-npr-chatbot-tia\Asset\logo.png")
+    col1, col2 = st.columns([1, 7])
+    with col1:
+        st.image(logo, width=80)
+    with col2:
+        st.title("Tanya TIA 👋")
+
+    st.write("TIA - Tel-U Interactive AI")
+
+    st.text_input("Tanya TIA seputar Pemrograman Berbasis Objek... ", key='query', on_change=submit)
+
+    user_input = st.session_state.user_input
+
+    st.write("You Entered...", user_input)
+
+    return user_input
+
+def render_response(result):
+    container_style = """
+    <style>
+    .response-container {
+        max-width: 700px;
+        margin: auto;
+    }
+    </style>
+    """
+    st.markdown(container_style, unsafe_allow_html=True)
+    st.markdown(f'<div class="response-container"><h2>TIA <span style="color: blue;">Menjawab...</span> 🗣️</h2><p>{result}</p></div>', unsafe_allow_html=True)
